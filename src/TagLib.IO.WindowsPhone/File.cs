fa1a2447b48b4e82cb0508ed8d3b36d5b5b78458
Andrey Rygin <andrey.rygin@live.ru>
2014-03-11 01:36:56 +0700

    Add Windows Phone and Windows Store IO libraries.

---
+++ src/TagLib.IO.WindowsPhone/File.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagLib.IO
+{
+	public class File
+	{
+		/// <summary>
+		///    Creates a new instance of a <see cref="File" /> subclass
+		///    for a specified path, guessing the mime-type from the
+		///    file's extension and using the average read style.
+		/// </summary>
+		/// <param name="path">
+		///    A <see cref="string" /> object specifying the file to
+		///    read from and write to.
+		/// </param>
+		/// <returns>
+		///    A new instance of <see cref="File" /> as read from the
+		///    specified path.
+		/// </returns>
+		/// <exception cref="CorruptFileException">
+		///    The file could not be read due to corruption.
+		/// </exception>
+		/// <exception cref="UnsupportedFormatException">
+		///    The file could not be read because the mime-type could
+		///    not be resolved or the library does not support an
+		///    internal feature of the file crucial to its reading.
+		/// </exception>
+		public static TagLib.File Create(string path)
+		{
+			return Create(path, null, ReadStyle.Average);
+		}
+
+		/// <summary>
+		///    Creates a new instance of a <see cref="File" /> subclass
+		///    for a specified path and read style, guessing the
+		///    mime-type from the file's extension.
+		/// </summary>
+		/// <param name="path">
+		///    A <see cref="string" /> object specifying the file to
+		///    read from and write to.
+		/// </param>
+		/// <param name="propertiesStyle">
+		///    A <see cref="ReadStyle" /> value specifying the level of
+		///    detail to use when reading the media information from the
+		///    new instance.
+		/// </param>
+		/// <returns>
+		///    A new instance of <see cref="File" /> as read from the
+		///    specified path.
+		/// </returns>
+		/// <exception cref="CorruptFileException">
+		///    The file could not be read due to corruption.
+		/// </exception>
+		/// <exception cref="UnsupportedFormatException">
+		///    The file could not be read because the mime-type could
+		///    not be resolved or the library does not support an
+		///    internal feature of the file crucial to its reading.
+		/// </exception>
+		public static TagLib.File Create(string path, ReadStyle propertiesStyle)
+		{
+			return Create(path, null, propertiesStyle);
+		}
+
+		/// <summary>
+		///    Creates a new instance of a <see cref="File" /> subclass
+		///    for a specified path, mime-type, and read style.
+		/// </summary>
+		/// <param name="path">
+		///    A <see cref="string" /> object specifying the file to
+		///    read from and write to.
+		/// </param>
+		/// <param name="mimetype">
+		///    A <see cref="string" /> object containing the mime-type
+		///    to use when selecting the appropriate class to use, or
+		///    <see langword="null" /> if the extension in <paramref
+		///    name="abstraction" /> is to be used.
+		/// </param>
+		/// <param name="propertiesStyle">
+		///    A <see cref="ReadStyle" /> value specifying the level of
+		///    detail to use when reading the media information from the
+		///    new instance.
+		/// </param>
+		/// <returns>
+		///    A new instance of <see cref="File" /> as read from the
+		///    specified path.
+		/// </returns>
+		/// <exception cref="CorruptFileException">
+		///    The file could not be read due to corruption.
+		/// </exception>
+		/// <exception cref="UnsupportedFormatException">
+		///    The file could not be read because the mime-type could
+		///    not be resolved or the library does not support an
+		///    internal feature of the file crucial to its reading.
+		/// </exception>
+		public static TagLib.File Create(string path, string mimetype, ReadStyle propertiesStyle)
+		{
+			return TagLib.File.Create(new PlatformFile(path), mimetype, propertiesStyle);
+		}
+	}
+}
---
+++ src/TagLib.IO.WindowsPhone/PlatformFile.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagLib.IO
+{
+	public class PlatformFile : TagLib.File.IFileAbstraction
+	{
+		/// <summary>
+		///    Contains the name used to open the file.
+		/// </summary>
+		private string name;
+
+		/// <summary>
+		///    Constructs and initializes a new instance of
+		///    <see cref="PlatformFile" /> for a
+		///    specified path in the local file system.
+		/// </summary>
+		/// <param name="path">
+		///    A <see cref="string" /> object containing the
+		///    path of the file to use in the new instance.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		///    <paramref name="path" /> is <see langword="null"
+		///    />.
+		/// </exception>
+		public PlatformFile(string path)
+		{
+			if (path == null)
+				throw new ArgumentNullException("path");
+
+			name = path;
+		}
+
+		/// <summary>
+		///    Gets the path of the file represented by the
+		///    current instance.
+		/// </summary>
+		/// <value>
+		///    A <see cref="string" /> object containing the
+		///    path of the file represented by the current
+		///    instance.
+		/// </value>
+		public string Name
+		{
+			get { return name; }
+		}
+
+		/// <summary>
+		///    Gets a new readable, seekable stream from the
+		///    file represented by the current instance.
+		/// </summary>
+		/// <value>
+		///    A new <see cref="System.IO.Stream" /> to be used
+		///    when reading the file represented by the current
+		///    instance.
+		/// </value>
+		public System.IO.Stream ReadStream
+		{
+			get
+			{
+				return System.IO.File.Open(Name,
+					System.IO.FileMode.Open,
+					System.IO.FileAccess.Read,
+					System.IO.FileShare.Read);
+			}
+		}
+
+		/// <summary>
+		///    Gets a new writable, seekable stream from the
+		///    file represented by the current instance.
+		/// </summary>
+		/// <value>
+		///    A new <see cref="System.IO.Stream" /> to be used
+		///    when writing to the file represented by the
+		///    current instance.
+		/// </value>
+		public System.IO.Stream WriteStream
+		{
+			get
+			{
+				return System.IO.File.Open(Name,
+					System.IO.FileMode.Open,
+					System.IO.FileAccess.ReadWrite);
+			}
+		}
+
+		/// <summary>
+		///    Closes a stream created by the current instance.
+		/// </summary>
+		/// <param name="stream">
+		///    A <see cref="System.IO.Stream" /> object
+		///    created by the current instance.
+		/// </param>
+		public void CloseStream(System.IO.Stream stream)
+		{
+			if (stream == null)
+				throw new ArgumentNullException("stream");
+
+			stream.Dispose();
+		}
+	}
+}
